f295d14ecb7fea14597a85c93648872df2fc0a71
Khurshid Ismoilov <95979088+khurshid28@users.noreply.github.com>
2026-02-09 02:08:21 +0500

    Add new CORS origin for external access

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,7 @@ async function bootstrap() {
   origin: [
     'http://localhost:8080',
     'http://127.0.0.1:8080',
+    'http://89.39.95.175:8080',
   ],
   credentials: true,
   methods: ['GET','POST','PUT','DELETE','PATCH','OPTIONS'],
