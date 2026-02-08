a57a95713a7538b9ce7a11cffb1b1412961f58dd
Khurshid Ismoilov <95979088+khurshid28@users.noreply.github.com>
2026-02-08 15:49:56 +0500

    Change CORS configuration to allow all origins

--- src/main.ts
+++ src/main.ts
@@ -19,9 +19,10 @@ async function bootstrap() {
  
   
   app.enableCors({
-    origin: ["http://127.0.0.1:8080" ,"http://localhost:8080" , "http://89.39.95.175:8080"],
-    credentials: true,
-
+    // origin: ["http://127.0.0.1:8080" ,"http://localhost:8080" , "http://89.39.95.175:8080"],
+    
+    // credentials: true,
+    origin :"*",
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
   });
